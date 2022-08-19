855244ec242c954fbb62ad0be46cb8d2f70aaef6
kriekkop <287479+Kriekkop@users.noreply.github.com>
2022-08-19 14:15:22 +0200

    Added button dummy functionality

--- src/app/components/network-filter/network-filter.component.ts
+++ src/app/components/network-filter/network-filter.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NetworkManagerComponent } from '../network-manager/network-manager.component';
 import { MatDialog } from '@angular/material/dialog';
 import { NetworkService } from '../../services/network.service';
@@ -9,14 +9,29 @@ import { NetworkService } from '../../services/network.service';
   styleUrls: ['./network-filter.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NetworkFilterComponent {
+export class NetworkFilterComponent implements OnInit {
   panelOpenState = true;
+  hiddenNetworks: string[] = [];
 
   constructor(public ns: NetworkService,
               public dialog: MatDialog) {
   }
 
+  ngOnInit(): void {
+    // Load initial settings for hidden networks.
+    return;
+  }
+
   openNetworkManager(): void {
     this.dialog.open(NetworkManagerComponent, {maxWidth: '100vw'});
+  }
+
+  toggleNetworkVisibility(name: string): void {
+    const index = this.hiddenNetworks.indexOf(name);
+    if (index === -1) {
+      this.hiddenNetworks.push(name);
+    } else {
+      this.hiddenNetworks.splice(1, index);
+    }
   }
 }
